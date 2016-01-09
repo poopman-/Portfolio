06213e1ddae41f909816a90639634d637f605912
poopman- <poopmanywot@gmail.com>
2016-01-09 17:11:40 -0500

    mobile detection

--- 3d-me.js
+++ 3d-me.js
@@ -1,16 +1,18 @@
 //3d-me.js created by Kyle Cypher
-$(document).ready(function(){
-function perspective(e, t) {
-    $(".3d-me").css({
-        transform: "perspective( 1000px ) rotateY(" + e + "deg) rotateX(" + t + "deg)"
-    });
-}
-$("body").bind("mousemove", function(e) {
-    var t = ({
-            width: $(window).width(),
-            height: $(window).height()
-        }, e.clientX - $(window).width() / 2),
-        i = e.clientY - $(window).height() / 2;
-    perspective(t / -100, i / 100);
+$(document).ready(function() {
+    function perspective(e, t) {
+        $(".3d-me").css({
+            transform: "perspective( 1000px ) rotateY(" + e + "deg) rotateX(" + t + "deg)"
+        });
+    }
+    if (!isMobileDevice) {
+        $("body").bind("mousemove", function(e) {
+            var t = ({
+                    width: $(window).width(),
+                    height: $(window).height()
+                }, e.clientX - $(window).width() / 2),
+                i = e.clientY - $(window).height() / 2;
+            perspective(t / -100, i / 100);
+        });
+    }
 });
-})
---
+++ ismobile.js
@@ -0,0 +1,39 @@
+var isMobileDevice = false;
+$(document).ready(function() {
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+	
+	
+	
+	
+if (isMobile.any()) {
+    isMobileDevice = true;
+	$("body").addClass("mobile")
+}
+
+
+window.screen.availHeight;
+window.screen.availWidth;
+
+
+});
+
